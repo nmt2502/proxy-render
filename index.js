b15d5881fa728876f9e7c4cf7aaa8536c92862ef
nmt2502 <anhlatuan2502@gmail.com>
2026-01-22 02:19:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,4 +15,28 @@ app.get("/api/sun", async (req, res) => {
   }
 });
 
+app.get("/api/bet", async (req, res) => {
+  try {
+    const r = await fetch("https://apidulieugame.onrender.com/api/dudoan/BET_THUONG");
+    const data = await r.json();
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.json(data);
+  } catch {
+    res.status(500).json({ error: true });
+  }
+});
+
+app.get("/api/68gb", async (req, res) => {
+  try {
+    const r = await fetch("https://apidulieugame.onrender.com/api/dudoan/68GB_MD5");
+    const data = await r.json();
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.json(data);
+  } catch {
+    res.status(500).json({ error: true });
+  }
+});
+
 app.listen(3000);
